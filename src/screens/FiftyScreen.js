ed878f10cb6acead94a03428066821f9760c353c
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-25 11:58:29 -0300

    filters for 50ml and homenaje

--- src/screens/FiftyScreen.js
+++ src/screens/FiftyScreen.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import WorkInProgress from '../components/WorkInProgress'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { listProducts } from '../actions/productActions';
 
-function FiftyScreen() {
+function HomenajeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { error, loading, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  // Filtrar productos que contienen la palabra 'homenaje'
+  const homenajeProducts = products.filter(product =>
+    product.name.toLowerCase().includes('50ml')
+  );
+
   return (
     <div>
-        <WorkInProgress />
+      <h1>Perfumes de 50ml</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {homenajeProducts.map(product => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
-  )
+  );
 }
 
-export default FiftyScreen
+export default HomenajeScreen;
