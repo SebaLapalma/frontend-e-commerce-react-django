f1e75a3b3c972e914e2554c2f115cb1a1d00c818
sebalapalma <e.sebastian.lapalma@gmail.com>
2024-04-25 21:57:30 -0300

    fixing routing problem with search page

--- src/screens/FiftyScreen.js
+++ src/screens/FiftyScreen.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Form } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -8,27 +8,37 @@ import { listProducts } from '../actions/productActions';
 
 function HomenajeScreen() {
   const dispatch = useDispatch();
-  const productList = useSelector(state => state.productList);
+  const productList = useSelector((state) => state.productList);
   const { error, loading, products } = productList;
+
+  const [keyword, setKeyword] = useState('');
 
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
-  const homenajeProducts = products.filter(product =>
-    product.name.toLowerCase().includes('50ml')
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(keyword.toLowerCase())
   );
 
   return (
     <div>
       <h1>Perfumes de 50ml</h1>
+      <Form>
+        <Form.Control
+          type="text"
+          placeholder="Buscar perfumes..."
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </Form>
       {loading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>{error}</Message>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
-          {homenajeProducts.map(product => (
+          {filteredProducts.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
